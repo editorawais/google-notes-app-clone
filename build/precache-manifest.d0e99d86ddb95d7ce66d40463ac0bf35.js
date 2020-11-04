cb07df4ce7bccd16dfc5e75ba1bf3be6ab17e6a0
editorawais <editorawais@gmail.com>
2020-11-04 13:50:05 +0100

    Your awesome message

---
+++ build/precache-manifest.d0e99d86ddb95d7ce66d40463ac0bf35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea847640aa057349d32cc36bbf5e5612",
+    "url": "/google-notes-app-clone/index.html"
+  },
+  {
+    "revision": "836f075d7bc926c91215",
+    "url": "/google-notes-app-clone/static/css/main.ab6eae2a.chunk.css"
+  },
+  {
+    "revision": "a43131fb5799b185f641",
+    "url": "/google-notes-app-clone/static/js/2.75b3fe92.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/google-notes-app-clone/static/js/2.75b3fe92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836f075d7bc926c91215",
+    "url": "/google-notes-app-clone/static/js/main.11ce2628.chunk.js"
+  },
+  {
+    "revision": "dcbfe8b853637c3731b6",
+    "url": "/google-notes-app-clone/static/js/runtime-main.1ef95bb8.js"
+  },
+  {
+    "revision": "dd717418f1a83826bb5c57a99ceae268",
+    "url": "/google-notes-app-clone/static/media/react.dd717418.svg"
+  }
+]);
